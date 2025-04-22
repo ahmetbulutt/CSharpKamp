763a7a53423a8cc718a93641c70049da0562d1a1
ahmetbulutt <ahmetsamet5@hotmail.com>
2025-04-22 23:42:52 +0300

    Add files via upload

---
+++ 1_MainSubjects/Program.cs
@@ -0,0 +1,176 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+#region merhabadünya
+
+//Console.WriteLine ("merhaba dünya");
+//Console.WriteLine ("selam");
+
+#endregion
+
+#region yazdırmakomutları
+
+//Console.WriteLine("******Yemek Menüsü*****");
+//Console.WriteLine();
+//Console.WriteLine("1-)Çorbalar");
+//Console.WriteLine("2-)Ana Yemekler");
+//Console.WriteLine("3-)Soğuk Başlangıçlar"); // ctrl+d yaparak bir satırı aşağı indirebilirsiniz
+//Console.WriteLine("4-)İçecekler");
+//Console.WriteLine("5-)Salatalar");
+//Console.WriteLine("6-)Tatlılar");
+//Console.WriteLine();
+//Console.WriteLine("******Yemek Menüsü*****");
+
+#endregion
+
+#region stringdeğişkenler
+
+//Değişkenler
+
+//string
+//String Komutunun İşlevleri:
+
+//Metin Depolama:
+//Stringler, adlar, adresler, açıklamalar gibi metin tabanlı verileri depolamak için kullanılır.
+//Metin İşleme:
+//Stringler üzerinde birleştirme, bölme, arama, değiştirme gibi çeşitli işlemler yapılabilir.
+//Veri Girişi/Çıktısı:
+//Kullanıcıdan metin girişi almak veya ekrana metin yazdırmak için stringler kullanılır.
+//Veri Tabanı İşlemleri:
+//Veri tabanlarında metin verilerini depolamak ve sorgulamak için string veri tipi kullanılır.
+//Dosya İşlemleri:
+//Dosyalardan metin okumak veya dosyalara metin yazmak için stringler kullanılır.
+//String Komutunun Kullanım Alanları:
+
+//Web Geliştirme:
+//Web sitelerinde kullanıcı girdilerini işlemek, dinamik içerik oluşturmak ve veri tabanıyla iletişim kurmak için stringler kullanılır.
+//Mobil Uygulama Geliştirme:
+//Mobil uygulamalarda kullanıcı arayüzü metinlerini, kullanıcı girdilerini ve uygulama verilerini işlemek için stringler kullanılır.
+//Veri Analizi:
+//Veri analizinde metin verilerini temizlemek, dönüştürmek ve analiz etmek için stringler kullanılır.
+//Oyun Geliştirme:
+//Oyunlarda karakter adlarını, oyun içi metinleri ve kullanıcı girdilerini işlemek için stringler kullanılır.
+//Yapay Zeka:
+//Doğal dil işleme (NLP) gibi yapay zeka uygulamalarında metin verilerini işlemek için stringler kullanılır.
+
+//string name;
+//name = "Ahmet";
+//Console.Write(name);
+
+//15 dakika boyunca endregion yazmayı unuttuğum için kod çalışmadı bir yere region ekliyorsan sonunda endregion at
+
+//string customerName;
+//string customerSurname;
+//string customerPhone;
+//string customerEmail,district, city;
+
+//customerName = "ahmet";
+//customerSurname = "bulut";
+//customerPhone = "+90 551 120 0256";
+//customerEmail = "ahmetsamet5@hotmail.com";
+//district = "sefaköy";
+//city = "istanbul";
+
+//Console.WriteLine("***Rezarvasyon***");
+//Console.WriteLine();
+//Console.WriteLine("Müşteri:" + customerName+ " " + customerSurname);
+//Console.WriteLine("iletişim:" + customerPhone);
+//Console.WriteLine("iletişim:" + customerEmail);
+//Console.WriteLine("Adres:" + district);
+//Console.WriteLine("Şehir:" + city);
+//Console.WriteLine("--------------------------------------------------------");
+////-----------------------------------------------------------------------------------------
+//customerName = "öykü";
+//customerPhone = "+90 555 444 1212";
+//customerSurname = "yazıcı";
+//customerEmail = "yarenmelek@gmail.com";
+//district = "avcılar";
+//city = "istanbul";
+//Console.WriteLine("Müşteri:"+ customerName);
+//Console.WriteLine("İletişim:" + customerPhone);
+//Console.WriteLine("Mail:" + customerEmail);
+//Console.WriteLine("Adres:"+ district + "/"+ city);
+
+
+
+
+
+
+
+
+
+#endregion;
+
+#region intdeğişkenler
+//int number = 24; 
+//Console.WriteLine(number); 
+
+using System.Runtime.CompilerServices;
+//intdeişkenlerkod
+//int hamburgerPrice = 300;
+//int cokePrice = 30;
+//int waterPrice = 10;
+//int piizaPrice = 250;
+//int lemonadePrice = 30;
+//Console.WriteLine("***Fiyatlar***");
+//Console.WriteLine("hamburger fiyatı:"+ hamburgerPrice+ " TL");
+//Console.WriteLine("kola fiyatı:"+ cokePrice+ " TL");
+//Console.WriteLine("su fiyatı:"+ waterPrice+ " TL");
+//Console.WriteLine("pizza fiyatı:"+ piizaPrice+ " TL");
+//Console.WriteLine("limonata fiyatı:" + lemonadePrice + " TL");
+//Console.WriteLine();
+//Console.WriteLine("---------------------------");
+//int hamburgerCount;
+//int cokeCount;
+//int waterCount;
+//int piizaCount;
+//int lemonadeCount;
+
+//int hamburgertotalPrice = 0;
+//int coketotalPrice = 0; 
+//int lemonadatotalPrice = 0;
+//int watertotalPrice = 0;
+//int piizatotalPrice = 0;
+
+//hamburgerCount = 5;
+//cokeCount = 3;
+//lemonadeCount = 2;
+//piizaCount = 1; 
+//waterCount = 6;
+
+//hamburgertotalPrice = hamburgerCount * hamburgerPrice;
+//coketotalPrice = cokeCount * cokePrice;
+//piizatotalPrice = piizaCount * piizaPrice;
+//watertotalPrice= waterCount * waterPrice;
+//lemonadatotalPrice = waterCount * waterPrice;
+    
+//Console.WriteLine("---------------------------");
+//Console.WriteLine("toplam hamburger: " + hamburgertotalPrice + " TL");
+//Console.WriteLine("toplam su: " + watertotalPrice + " TL");
+//Console.WriteLine("toplam pizza:" + piizatotalPrice + " TL");
+//Console.WriteLine("toplam kola:" + coketotalPrice+ " TL");
+//Console.WriteLine("toplam limonnata:" + lemonadatotalPrice+ " TL");
+
+//Console.WriteLine();
+//Console.WriteLine("---------------------------");
+
+//int totalPrice =coketotalPrice+ hamburgertotalPrice+ watertotalPrice+ piizatotalPrice+ lemonadatotalPrice;
+//Console.WriteLine("Toplam Yemek Tutarı:"+ totalPrice);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endregion
